559192066d8f1aa75c5e0867a6eef655601eb95b
bakebrlk <210103194@stu.sdu.edu.kz>
2024-05-08 23:09:37 +0500

    Initial Commit

---
+++ AvocadoKZ/AvocadoKZApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoKZApp.swift
+//  AvocadoKZ
+//
+//  Created by bakebrlk on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoKZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
