6e92cd667732d297eec7de670d043e9e5efce995
bakebrlk <210103194@stu.sdu.edu.kz>
2024-05-08 23:29:40 +0500

    add Data and HintPage view

---
+++ AvocadoKZ/source/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  AvocadoKZ
+//
+//  Created by bakebrlk on 08.05.2024.
+//
+
+import Foundation
---
+++ AvocadoKZ/view/HintPageView.swift
@@ -0,0 +1,21 @@
+//
+//  HintPageView.swift
+//  AvocadoKZ
+//
+//  Created by bakebrlk on 08.05.2024.
+//
+
+import SwiftUI
+
+struct HintPageView: View {
+        
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    HintPageView()
+}
