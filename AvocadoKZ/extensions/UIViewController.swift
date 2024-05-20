455b6b750231b3a427b7f30fbe31a2c3932b04f8
bakebrlk <210103194@stu.sdu.edu.kz>
2024-05-21 01:18:39 +0500

    integrate native present

---
+++ AvocadoKZ/extensions/UIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController.swift
+//  AvocadoKZ
+//
+//  Created by bakebrlk on 21.05.2024.
+//
+
+import SwiftUI
+
+extension UIViewController {
+    func present<Content: View>(style: UIModalPresentationStyle = .automatic, @ViewBuilder builder: () -> Content) {
+        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
+        toPresent.modalPresentationStyle = style
+        toPresent.rootView = AnyView(
+            builder()
+                .environment(\.viewController, toPresent)
+        )
+        NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "dismissModal"), object: nil, queue: nil) { [weak toPresent] _ in
+            toPresent?.dismiss(animated: true, completion: nil)
+        }
+        self.present(toPresent, animated: true, completion: nil)
+    }
+}
